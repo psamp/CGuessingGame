70ac96ba19407b1d4565ee0d3950c7dfe533fe11
Princess Sampson <ps@kingonomics.com>
2016-08-15 18:55:33 -0400

    Allow user to enter their name, guess, and if they're wrong, show them their guess(es) and allow them to try again.

--- GuessingGame/main.m
+++ GuessingGame/main.m
@@ -8,28 +8,57 @@
 
 #import <Foundation/Foundation.h>
 
+int getNumberFromUser(int maxValidChoice);
+
 int main(int argc, const char * argv[]) {
     
     NSNumber *number = @(arc4random_uniform(10));
-    NSNumber *guess;
-    NSMutableArray *previousGuesses;
-    NSString *name;
+    NSMutableArray *allGuesses  = [NSMutableArray array];
+    bool anyGuessesRight = false;
     
-    int cNum = -1;
-    int numbersScannedIn = -1;
-    
-    while (numbersScannedIn == -1) {
-        numbersScannedIn = scanf("%d", &cNum);
+    for (int i = 0; i < 3; i++) {
+        
+        NSLog(@"I'm thinking of a number 0-9. You're on guess %d/3.", i + 1);
+        [allGuesses addObject: @(getNumberFromUser(9))];
     }
     
-    guess = @(cNum);
+    for (NSNumber *guess in allGuesses) {
+        
+        if ([guess isEqual: number]) {
+            anyGuessesRight = true;
+        }
+    }
     
-    if (guess == number) {
-        NSLog(@"You're right! I'm thinking of %@\n\n", number);
+    if (!anyGuessesRight) {
+        NSLog(@"Wrong! I'm thinking of %@.", number);
+        NSLog(@"You guessed: %@", allGuesses);
     } else {
-    NSLog(@"Wrong! I'm thinking of %@\n\n", number);
+        NSLog(@"Right! I was thinking of %@.", number);
     }
-    
     
     return 0;
+}
+
+bool getStringFromUser(char name[], int nameLength) {
+    char *result = NULL;
+    while (result != name) {
+        fpurge(stdin);
+        result = fgets(name, nameLength, stdin);
+    }
+    
+    return true;
+}
+
+int getNumberFromUser(int maxValidChoice) {
+    int choice = 0;
+    int numberOfItemsScanned = 0;
+    int firstValidChoice = 0;
+    
+    fpurge(stdin);
+    numberOfItemsScanned = scanf("%d", &choice);
+    if ((numberOfItemsScanned == 1) && (choice >= firstValidChoice) && (choice <= maxValidChoice)) {
+        return choice;
+    }
+    
+    return -1;
 }
