6201ea205f4b8a24a366fb6e97bd123bca28e692
Princess Sampson <ps@kingonomics.com>
2016-08-15 16:34:17 -0400

    Will add the ability to enter a number and see if it matches a random num 0-9

--- GuessingGame/main.m
+++ GuessingGame/main.m
@@ -9,9 +9,27 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+    
+    NSNumber *number = @(arc4random_uniform(10));
+    NSNumber *guess;
+    NSMutableArray *previousGuesses;
+    NSString *name;
+    
+    int cNum = -1;
+    int numbersScannedIn = -1;
+    
+    while (numbersScannedIn == -1) {
+        numbersScannedIn = scanf("%d", &cNum);
     }
+    
+    guess = @(cNum);
+    
+    if (guess == number) {
+        NSLog(@"You're right! I'm thinking of %@\n\n", number);
+    } else {
+    NSLog(@"Wrong! I'm thinking of %@\n\n", number);
+    }
+    
+    
     return 0;
 }
